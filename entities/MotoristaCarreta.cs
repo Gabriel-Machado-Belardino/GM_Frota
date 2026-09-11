140ec1585c948d5e5f4f4323b6251112783fbe9a
Gustavo-Meroni <gustavopmeroni@gmail.com>
2026-09-11 18:06:52 -0300

    feat(entities): implementar EntregadorMoto com atributo ZonaEntrega e override de MostrarDetalhes

--- entities/MotoristaCarreta.cs
+++ entities/MotoristaCarreta.cs
@@ -1,11 +1,39 @@
-﻿using GM_Frota.interfaces;
+using GM_Frota.interfaces;
+using GM_Frota.utils;
 
 namespace GM_Frota.entities
 {
     internal class MotoristaCarreta : FuncionarioTransporte, IMotoristaCarreta
     {
-        public MotoristaCarreta(string Nome, string Registro) : base(Nome, Registro)
+        private string categoriaCnh = string.Empty;
+
+        public MotoristaCarreta(string nome, string registro, Carreta carreta, string categoriaCnh)
+            : base(nome, registro, carreta)
         {
+            this.categoriaCnh = categoriaCnh;
+        }
+
+        public string CategoriaCnh
+        {
+            get { return categoriaCnh; }
+            set { categoriaCnh = value; }
+        }
+
+        public override void MostrarDetalhes()
+        {
+            base.MostrarDetalhes();
+            ConsoleWriter.WriteLine("Categoria CNH", this.CategoriaCnh);
+            ConsoleWriter.WriteEmptyLine();
+            ConsoleWriter.WriteLine("Placa da Carreta", this.Veiculo.Placa);
+            ConsoleWriter.WriteEmptyLine();
+
+            if (this.Veiculo is ICarreta carreta)
+            {
+                ConsoleWriter.WriteLine("Peso Suportado (kg)", carreta.PesoSuportado.ToString());
+                ConsoleWriter.WriteEmptyLine();
+            }
+
+            ConsoleWriter.WriteEnd();
         }
     }
 }
--- interfaces/IMotoristaCarreta.cs
+++ interfaces/IMotoristaCarreta.cs
@@ -2,5 +2,6 @@
 {
     internal interface IMotoristaCarreta : IFuncionarioTransporte
     {
+        string CategoriaCnh { get; set; }
     }
 }
